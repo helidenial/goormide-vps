c34046ae0e8fcc8e0f175fc596d9a41b539d2a0b
helidenial <helidenial8@gmail.com>
2022-01-01 22:01:28 +0530

    First commit

---
+++ command.sh
@@ -0,0 +1,18 @@
+
+sudo apt update 
+sudo apt -y install vnc4server
+
+sudo apt install lxde
+echo ######################################################################################################
+echo running commands: vncpasswd
+echo '123456\n123456'| vncpasswd
+vncserver -geometry 1366x768
+vncserver -kill :1
+
+mv ~/.vnc/xstartup ~/.vnc/xstartup.bak
+mv xstartup /root/.vnc/xstartup
+
+vncserver 
+sudo apt-get install firefox
+
+
